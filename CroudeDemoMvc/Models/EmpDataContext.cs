765fd28249fe3beed27382d6a051178f57ef7562
Admin <Admin@DESKTOP-N5ABTTO>
2024-07-05 15:04:15 +0530

    Add project files.

---
+++ CroudeDemoMvc/Models/EmpDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CroudeDemoMvc.Models
+{
+    public class EmpDataContext : DbContext
+    {
+        public EmpDataContext(DbContextOptions<EmpDataContext> options) : base(options)
+        {
+            
+        }
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<Image> Image { get; set; }
+        
+    }
+}
---
+++ CroudeDemoMvc/Models/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace CroudeDemoMvc.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Photo { get; set; }
+    }
+}
